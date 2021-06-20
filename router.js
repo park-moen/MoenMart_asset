808675b7143401750a6ae455867435f06b77e5e5
park-moen <pmj6516@naver.com>
2021-06-20 18:07:12 +0900

    feat: Data fetching the Handlebars
    - rest -
    Create DB
    Connect to DB

--- router.js
+++ router.js
@@ -2,12 +2,59 @@ const mainViewTemplate = require('./pages/mainView.hbs');
 const itemListTemplate = require('./pages/itemList.hbs');
 const lookbookTemplate = require('./pages/lookBook.hbs');
 
-const render1 = require('./src/api/shop');
-
-console.log(render1());
+const data = [
+  {
+    id: 1,
+    title: 'Algot',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9023cd4f660d540f79a24b_Algot.png',
+    price: '0.99',
+  },
+  {
+    id: 2,
+    title: 'Evert',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9022747d535f99eba26e0e_William.png',
+    price: '0.99',
+  },
+  {
+    id: 3,
+    title: 'Ingvar',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9022747d535f6145a26e0d_Vilda.png',
+    price: '0.99',
+  },
+  {
+    id: 4,
+    title: 'Roald',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b902274e41c3c3e9d489b58_Roald.png',
+    price: '0.99',
+  },
+  {
+    id: 5,
+    title: 'William',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9023cd4f660d540f79a24b_Algot.png',
+    price: '0.99',
+  },
+  {
+    id: 6,
+    title: 'Harald',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9023cd4f660d540f79a24b_Algot.png',
+    price: '0.99',
+  },
+  {
+    id: 7,
+    title: 'Alva',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9023cd4f660d540f79a24b_Algot.png',
+    price: '0.99',
+  },
+  {
+    id: 8,
+    title: 'Vilda',
+    img: 'https://uploads-ssl.webflow.com/5b63ff5acd934c35bc633385/5b9023cd4f660d540f79a24b_Algot.png',
+    price: '0.99',
+  },
+];
 
 const MainView = mainViewTemplate();
-const ItemList = itemListTemplate();
+const ItemList = itemListTemplate(data);
 const LookBook = lookbookTemplate();
 
 const routes = {
--- src/api/shop.js
+++
@@ -1,9 +0,0 @@
-const itemListTemplate = require('../../pages/itemList.hbs');
-
-const ItemList = itemListTemplate();
-
-function render1() {
-  console.log(ItemList);
-}
-
-module.exports = render1;
